fd81a52228bad5ab5d784649fe458ff248dd0145
Ashish Kumar Satyam <ashishsatyam.kmr@gmail.com>
2018-03-22 08:54:43 +0530

    add industry material page updated in client app

--- src/pages/AddIndustryMaterial/AddIndustryMaterial.ts
+++ src/pages/AddIndustryMaterial/AddIndustryMaterial.ts
@@ -11,8 +11,12 @@ export class AddIndustryMaterialPage {
   public title:any="Add Materials";
   public MaterialName:any='';
   public Description:any='';
-  public inspectionPara='';
-  public qualityCheckPara='';
+  public minParem:any=0;
+  public maxParem:any=100;
+  public ParemCount:any=this.minParem;
+  public paremeterName : any= [];
+  public paremeterQuantity : any= [];
+  public tempparemeterName:Array<{id: number, value: string}> =[];  
   constructor(public navCtrl: NavController,
         public navParms: NavParams,
         public webService: ServiceSingletonProvider) {
@@ -21,8 +25,20 @@ export class AddIndustryMaterialPage {
         }
   }
 
+  onInputChange(){
+    console.log(this.ParemCount);
+    this.paremeterName=[];
+    this.paremeterQuantity=[]
+    this.tempparemeterName=[];
+    if(this.ParemCount>0){
+      for(let j=0;j<this.ParemCount;j++){
+        this.tempparemeterName.push({id:j,value:""+j});
+      }
+    }
+  }
+
   SaveMaterial(){
-    if(this.MaterialName=='' && this.Description=='' && this.inspectionPara=='' && this.qualityCheckPara=='' ){
+    if(this.MaterialName=='' && this.Description==''){
       this.webService.presentAlert('Alert!','Fill all the blank fields.');
     }
     else if(this.MaterialName==''){
@@ -31,26 +47,45 @@ export class AddIndustryMaterialPage {
     else if(this.Description==''){
       this.webService.presentAlert('Alert!','Enter the material description');
     }
-    else if(this.inspectionPara==''){
-      this.webService.presentAlert('Alert!','Enter the material inspection paremater');
-    }
-    else if(this.qualityCheckPara==''){
-      this.webService.presentAlert('Alert!','Enter the material qualitycheck paremeter');
+    else if(this.ParemCount>0 && this.tempparemeterName.length>0){
+      for(let v=0;v<this.tempparemeterName.length;v++){
+        if(this.paremeterName[v]==undefined || !this.paremeterName[v]){
+            return this.webService.presentAlert('Alert!','Enter '+(v+1)+' Paremeter Name');
+        }
+        else if(this.paremeterQuantity[v]==undefined || !this.paremeterQuantity[v]){
+          return this.webService.presentAlert('Alert!','Enter '+(v+1)+' Paremeter PerUnit Quantity');          
+        }
+      }
+      this.postData();
     }
     else{
-      this.postData();
+    this.postData();
     }
   }
 
   Reset(){
     this.MaterialName='';
     this.Description='';
-    this.inspectionPara='';
-    this.qualityCheckPara='';
+    this.ParemCount=0;
+    this.tempparemeterName=[];
   }
 
   postData(){
-    var myKeyVals={indu_id:this.IndustryId, name:this.MaterialName, description:this.Description, inspection_parem:this.inspectionPara, qulaitycheck_parem:this.qualityCheckPara};
+    let paremeterArr=[];
+    let myKeyVals={indu_id:this.IndustryId, name:this.MaterialName, description:this.Description};
+    if(this.tempparemeterName.length){
+      let k=0;
+      for(let item of this.tempparemeterName){
+        let obj={
+          parem_name:this.paremeterName[k],
+          parem_qty:Number(this.paremeterQuantity[k])
+        }
+        paremeterArr.push(obj);
+        k++;
+      }
+      myKeyVals['parameters']=paremeterArr;
+    }
+    console.log(myKeyVals);
     this.webService.presentLoading();
     this.webService.postIndustryOneMaterial(myKeyVals).then(data=>{
       this.webService.stopLoading();
